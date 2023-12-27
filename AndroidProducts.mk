a2654b5a5490bebf6ccb61e820e40eaeadf6b572
Mourya Baruah <mouryabaruah@gmail.com>
2023-12-27 12:40:58 +0530

    adapt for ProjectBlaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sky.mk
+    $(LOCAL_DIR)/blaze_sky.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sky-eng \
-    aosp_sky-userdebug \
-    aosp_sky-user
+    blaze_sky-eng \
+    blaze_sky-userdebug \
+    blaze_sky-user
