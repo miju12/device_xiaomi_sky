82008415b8e1d5f15ec290147c673292c7524c5d
madmax7896 <madmax78968@gmail.com>
2025-04-02 15:55:49 +0530

    Import Dolby audio blobs and manifest Squashed with:
    commit c525bc1923414b672d08d01c47a66dba3c8eeeb1
    Author: ShevT <evgeny.shishkov@gmail.com>
    Date:   Wed Nov 27 14:23:02 2024 +0300
    
        sm8150-common: Dolby: Add manifest
    
    commit 0a7aab81667f137c57fa95c4661fa7e43e0e2c1c
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Fri Aug 2 20:27:52 2024 +0530
    
        sweet: Move dax-default to device tree
    
        Change-Id: Id2054bb5e61bcfe58706e32f44d37fe7524aa912
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -96,7 +96,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ravelin/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ravelin/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ravelin_qssi/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ravelin_qssi/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
