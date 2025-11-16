a70884eeb6b8960420fd8569d06752f2694fd320
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    sky: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0

--- device.mk
+++ device.mk
@@ -100,6 +100,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
 # Bluetooth
+
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl:64
 
