90798fb45050bbf22e9729ba80a426f109fc66f7
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sky: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -63,6 +63,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     'vendor/lib64/nfc_nci.nqx.default.hw.so': blob_fixup()
 	.add_needed('libbase_shim.so'),
     (
