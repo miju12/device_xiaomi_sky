5eab2f55e9c65a52f4026a09e3c5701382a9ddc3
miju12 <mouryabaruah@gmail.com>
2026-03-27 16:56:02 +0000

    Revert "sky: Setup DeviceAsWebcam"
    This reverts commit 2f79bf7ef732043b25c8ecfb1f5ec76cf4208c33.

--- device.mk
+++ device.mk
@@ -334,8 +334,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-  $(DEVICE_PATH)/overlay \
-  $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_PACKAGES += \
