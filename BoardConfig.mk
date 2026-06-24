a8ed9c04692c6e361fa48854b9ac761f43123e2a
Frost <frost.github@proton.me>
2026-06-24 14:12:38 +0530

    sky: Use clang-r563880c for now
    Change-Id: Icd67ae51e3e63777e769613b89177525cf9859b5
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,9 @@ $(call soong_config_set, qtilocation, feature_nhz, false)
 HWUI_COMPILE_FOR_PERF := true
 
 # Kernel
+TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
 
